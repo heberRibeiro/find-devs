d0526dade6c1a72a0793d0636d74dfc547a40088
Heber Ribeiro <heber.r@outlook.com>
2020-02-04 16:39:23 -0300

    Projeto iniciado

---
+++ backendfinddevs/src/main/java/com/heber/backendfinddevs/BackendfinddevsApplication.java
@@ -0,0 +1,13 @@
+package com.heber.backendfinddevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfinddevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfinddevsApplication.class, args);
+	}
+
+}
---
+++ backendfinddevs/src/test/java/com/heber/backendfinddevs/BackendfinddevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heber.backendfinddevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfinddevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
