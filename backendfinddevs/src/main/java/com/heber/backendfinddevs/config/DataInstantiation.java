f64600e0255011005a2677acd5a7d151e8d7916e
Heber Ribeiro <heber.r@outlook.com>
2020-02-04 17:58:08 -0300

    Carga inicial de dados

---
+++ backendfinddevs/src/main/java/com/heber/backendfinddevs/config/DataInstantiation.java
@@ -0,0 +1,32 @@
+package com.heber.backendfinddevs.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.heber.backendfinddevs.domain.Dev;
+import com.heber.backendfinddevs.repository.DevRepository;
+
+/*** Classe de configuração para carga inicial de dados ***/
+
+@Configuration
+public class DataInstantiation implements CommandLineRunner {
+
+	@Autowired
+	private DevRepository devRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		devRepository.deleteAll();
+
+		Dev joao = new Dev(null, "João da Silva", "joaosilva", "Desenvolvedor web", "");
+		Dev maria = new Dev(null, "Maria da Silva", "mariasilva", "Desenvolvedora web", "");
+		Dev jose = new Dev(null, "José da Silva", "josesilva", "Desenvolvedor web", "");
+
+		devRepository.saveAll(Arrays.asList(joao, maria, jose));
+	}
+
+}
