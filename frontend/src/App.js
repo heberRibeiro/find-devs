bb85afa2ace0d2c2d454430ecebfa0c05c24447f
Heber Ribeiro <heber.r@outlook.com>
2020-02-17 20:02:26 -0300

    Estruturado o formulário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
+import './Global.css'
+import './App.css'
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form method="post">
+
+          <label htmlFor="github_username">Usuário do GitHub</label>
+          <input name="github_username" id="github_username" />
+
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" />
+
+          <label htmlFor="link">Link do GitHub</label>
+          <input name="link" id="link" />
+
+          <div className="coordinates">
+            <div className="longitude">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" />
+            </div>
+            <div className="latitude">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
