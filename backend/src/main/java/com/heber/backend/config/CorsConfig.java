5527124aad3165650e1a6fdee7ad343c778386e2
Heber Ribeiro <heber.r@outlook.com>
2020-04-01 14:30:51 -0300

    Configuração de CORS

--- backend/src/main/java/com/heber/backend/config/CorsConfig.java
+++ backend/src/main/java/com/heber/backend/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.heber.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
 	/*** CORS Configuration ***/
+	@Profile("dev")
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT")
