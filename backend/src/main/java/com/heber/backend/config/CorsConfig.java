17a44ceb165fdc4a22cfd4a3a214523872fef11b
Heber Ribeiro <heber.r@outlook.com>
2020-04-03 19:53:55 -0300

    Ajuste nos ambientes de desenvolvimento

--- backend/src/main/java/com/heber/backend/config/CorsConfig.java
+++ backend/src/main/java/com/heber/backend/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.heber.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,12 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-	/*** CORS Configuration ***/
-	@Profile("dev")
+	/*===== CORS CONFIGURATION =====*/
+	
+	/*
+	 * In the development profile, the react frontend application runs locally on port 3000, 
+	 * and it is on this host and port that CORS in that profile is enabled.
+	 * When running in a container, the React frontend application can be accessed locally (Docker desktop), 
+	 * or it can be accessed via the docker machine's ip (Docker toolbox). Therefore, CORS is enabled for both hosts.
+	 * 
+	 */
+
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-				.allowedOrigins("http://192.168.99.100:3000")
+				.allowedOrigins("http://localhost:3000", "http://192.168.99.100:3000")
 				.allowedHeaders("Content-Type", "Accept")
 				.exposedHeaders("Content-Length");
 	}
